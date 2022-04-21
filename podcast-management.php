d643f88dea88d14d220c7104a6c8e9c41adf9a64
Will Skora <skorasaurus@gmail.com>
2022-04-21 14:10:31 -0400

    initial

---
+++ podcast-management.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace cpl\podcast_management;
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://skorasaur.us
+ * @since             1.0.1
+ * @package          Podcast Episode Management
+ *
+ * @wordpress-plugin
+ * Plugin Name:       CPL Podcast Episode Management
+ * Plugin URI:        https://gitlab.com/cpl/cpl-podcast-management
+ * Description:       Registering OCFTB's Podcasts
+ * Requires PHP:      7.2+
+ * Version:           1.1.0
+ * Author:            Will Skora & The CPL Team
+ * Author URI:        https://gitlab.com/cpl/cpl-podcast-management
+ * License:           GPL-3.0+
+ * License URI:       https://choosealicense.com/licenses/gpl-3.0/
+ * Text Domain:       cpl-podcast-management
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+
+// not sure if this a standard?
+define( 'CPL_POD_MGMT_VERSION', '1.1.8' );
+
+// set the base directory
+define( 'CPL_POD_MGMT_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+
+require_once( CPL_POD_MGMT_PLUGIN_DIR . 'register-post-type.php' );
+
+
+add_action( 'init', __NAMESPACE__ . '\cpl_register_podcast_episode' );
+
+// https://codex.wordpress.org/Function_Reference/register_post_type#Flushing_Rewrite_on_Activation
+// This is to flush rewrites so if I decide to change the path of the CPT, it will auotmatically
+// do it on plugin activation
+function my_rewrite_flush() {
+	// First, we "add" the custom post type via the above written function.
+	// Note: "add" is written with quotes, as CPTs don't get added to the DB,
+	// They are only referenced in the post_type column with a post entry,
+	// when you add a post of this CPT.
+	cpl_register_podcast_episode();
+
+	// ATTENTION: This is *only* done during plugin activation hook in this example!
+	// You should *NEVER EVER* do this on every page load!!
+	\flush_rewrite_rules();
+}
+register_activation_hook( __FILE__, __NAMESPACE__ . '\my_rewrite_flush' );
---
+++ register-post-type.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace cpl\podcast_management;
+
+function cpl_register_podcast_episode() {
+
+	$labels = array(
+		'name'                  => _x( 'Podcast Episodes', 'Post Type General Name', 'cpl-podcast-management' ),
+		'singular_name'         => _x( 'Podcast Episode', 'Post Type Singular Name', 'cpl-podcast-management' ),
+		'menu_name'             => __( 'Podcast Episodes', 'cpl-podcast-management' ),
+		'name_admin_bar'        => __( 'Podcast Episode', 'cpl-podcast-management' ),
+		'archives'              => __( 'Podcast Episode Archives', 'cpl-podcast-management' ),
+		'attributes'            => __( 'Podcast Episode Attributes', 'cpl-podcast-management' ),
+		'parent_item_colon'     => __( 'Parent Podcast Episode:', 'cpl-podcast-management' ),
+		'all_items'             => __( 'All Podcast Episodes', 'cpl-podcast-management' ),
+		'add_new_item'          => __( 'Add New Podcast Episode', 'cpl-podcast-management' ),
+		'add_new'               => __( 'Add New', 'cpl-podcast-management' ),
+		'new_item'              => __( 'New Podcast Episode', 'cpl-podcast-management' ),
+		'edit_item'             => __( 'Edit Podcast Episode', 'cpl-podcast-management' ),
+		'update_item'           => __( 'Update Podcast Episode', 'cpl-podcast-management' ),
+		'view_item'             => __( 'View Podcast Episode', 'cpl-podcast-management' ),
+		'view_items'            => __( 'View Podcast Episodes', 'cpl-podcast-management' ),
+		'search_items'          => __( 'Search Podcast Episode', 'cpl-podcast-management' ),
+		'not_found'             => __( 'Not found', 'cpl-podcast-management' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'cpl-podcast-management' ),
+		'featured_image'        => __( 'Featured Image', 'cpl-podcast-management' ),
+		'set_featured_image'    => __( 'Set featured image', 'cpl-podcast-management' ),
+		'remove_featured_image' => __( 'Remove featured image', 'cpl-podcast-management' ),
+		'use_featured_image'    => __( 'Use as featured image', 'cpl-podcast-management' ),
+		'insert_into_item'      => __( 'Insert into item', 'cpl-podcast-management' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'cpl-podcast-management' ),
+		'items_list'            => __( 'Podcast Episodes list', 'cpl-podcast-management' ),
+		'items_list_navigation' => __( 'Podcast Episodes list navigation', 'cpl-podcast-management' ),
+		'filter_items_list'     => __( 'Filter items list', 'cpl-podcast-management' ),
+	);
+	$rewrite = array(
+		'slug'                  => 'podcast',
+		'with_front'            => true,
+		'pages'                 => true,
+		'feeds'                 => true,
+	);
+	$args = array(
+		'label'                 => __( 'Podcast Episode', 'cpl-podcast-management' ),
+		'description'           => __( 'Podcast episode', 'cpl-podcast-management' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail' ),
+		'taxonomies'            => array( ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'delete_with_user'    	=> false,
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'rewrite'               => $rewrite,
+		'capability_type'       => 'post',
+		'show_in_rest'          => true,
+	);
+	register_post_type( 'podcast_episode', $args );
+
+}
