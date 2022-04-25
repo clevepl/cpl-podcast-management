f91012714b79d347a3b62a1143ac2bce4ac9662a
Will Skora <skorasaurus@gmail.com>
2022-04-25 12:31:20 -0400

    do not create archives page

--- register-post-type.php
+++ register-post-type.php
@@ -54,7 +54,7 @@ function cpl_register_podcast_episode() {
 		'show_in_admin_bar'   => true,
 		'show_in_nav_menus'   => true,
 		'can_export'          => true,
-		'has_archive'         => true,
+		'has_archive'         => false,
 		'exclude_from_search' => false,
 		'publicly_queryable'  => true,
 		'rewrite'             => $rewrite,
