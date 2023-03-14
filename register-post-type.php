8ad2d1e91b007c49f618eb73a428cbe3f849184c
Will Skora <skorasaurus@gmail.com>
2023-03-14 13:10:54 -0400

    tweak registration params

--- register-post-type.php
+++ register-post-type.php
@@ -45,22 +45,23 @@ function cpl_register_podcast_episode() {
 		'labels'              => $labels,
 		'supports'            => array( 'title', 'editor', 'thumbnail' ),
 		'taxonomies'          => array(),
-		'hierarchical'        => false,
-		'public'              => true,
-		'show_ui'             => true,
-		'show_in_menu'        => true,
-		'delete_with_user'    => false,
-		'menu_position'       => 5,
-		'show_in_admin_bar'   => true,
-		'show_in_nav_menus'   => true,
 		'can_export'          => true,
-		'has_archive'         => false,
+		'capability_type'     => 'post',
+		'delete_with_user'    => false,
 		'exclude_from_search' => false,
+		'has_archive'         => false,
+		'hierarchical'        => false,
+		'menu_position'       => 5,
+		'public'              => true,
 		'publicly_queryable'  => true,
 		'rewrite'             => $rewrite,
-		'capability_type'     => 'post',
+		'show_in_admin_bar'   => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
 		'show_in_rest'        => true,
+		'show_ui'             => true,
 	);
+
 	register_post_type( 'podcast_episode', $args );
 
 }
