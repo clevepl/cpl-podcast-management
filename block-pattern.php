156f1ad40bae69147869e870dc076fd46a954f23
Will Skora <skorasaurus@gmail.com>
2023-03-14 13:10:24 -0400

    add category to the block pattern

--- block-pattern.php
+++ block-pattern.php
@@ -2,6 +2,15 @@
 
 namespace cpl\podcast_management;
 
+add_action( 'init', __NAMESPACE__ . '\register_cpl_block_pattern_categories' );
+
+function register_cpl_block_pattern_categories() {
+	register_block_pattern_category(
+		'ocftb',
+		array( 'label' => __( 'OCFTB', 'cpl-podcast-management' ) )
+	);
+}
+
 
 function cpl_register_block_patterns() {
 	register_block_pattern(
@@ -9,6 +18,7 @@ function cpl_register_block_patterns() {
 		array(
 			'title'       => __( 'Podcast Episode Pattern', 'cpl-podcast-management' ),
 			'description' => _x( 'contains several blocks for the podcast template', 'Block pattern description', 'cpl-podcast-management' ),
+			'categories' => array( 'ocftb' ),
 			'content'     => '<!-- wp:html /-->
 
 		<!-- wp:heading -->
